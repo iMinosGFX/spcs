75661303fe520bb2d6b17c3949b86d33d2922865
Haxaire <haxaire@opta-lp.com>
2021-06-20 16:40:11 +0200

    produits dans les storages

--- src/routes/Pages/Storages/StoragesList.tsx
+++ src/routes/Pages/Storages/StoragesList.tsx
@@ -1,3 +1,5 @@
+ //@ts-nocheck 
+
 import React, {useState, useEffect, useRef} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import StoragesAPI from '../../../api/StoragesAPI';
@@ -8,7 +10,7 @@ import { UserState } from '../../../store/user/reducer';
 import { CombinedStocks, ExtractStorageContent, ExtractStoragesWihStocks, ExtractStorageWithStocksContent } from '../../../@types/storages';
 import ServerSideTable from '../../../components/Tables/ServerSideTable';
 import { FilterItem } from '../../../components/Tables/FiltersInteract';
-
+import { createPrinter } from 'typescript';
 const filterColumns: FilterItem[] = [
 ]
 
@@ -27,12 +29,12 @@ const StoragesList = () => {
           accessor: 'id.dateExpiration',
         },
         {
-          Header: 'Prix',
-          accessor: 'price',
+            Header: 'Quantité',
+            accessor: 'totalQuantity',
         },
         {
-          Header: 'Quantité',
-          accessor: 'totalQuantity',
+          Header: 'Prix',
+          accessor: 'price',
         }
     ]
 
@@ -40,33 +42,36 @@ const StoragesList = () => {
     const [searchProduct, setsearchProduct] = useState<string>(undefined)
     const [loading, setLoading] = useState<boolean>(true)
     const ServerSideTableRef = useRef();
-    const [data, setData] = useState<ExtractStorageWithStocksContent>(null)
 
     const [storagesList, setStoragesList] =  useState<{label:string, value: number}[]>([])
     const [selectedStorage, setSelectedStorage] = useState<{value:number, label:string}>(null)
     const {connectedUser} = useSelector<UserState, UserState>((state: UserState) => state)
+    const [stocks, setStocks] = useState<ExtractStoragesWihStocks>(null)
 
     useEffect(() => {
         dispatch(clearBreadCrumb())
         dispatch(setContentTitle('Stockages'))
         dispatch(setSecondaryNav("none"))
 
-        StoragesAPI.findAllStoragesLinkToUser(connectedUser.id).then(data => setStoragesList(data.map(storage => ({value: storage.id, label: storage.name}))))
+        StoragesAPI.findAllStoragesLinkToUser(connectedUser.id)
+        .then(data => setStoragesList(data.map(storage => ({value: storage.id, label: storage.name}))))
 
     }, [])
 
     const getData = () => {
     	StoragesAPI.readStorageWithStocks(3)
     	.then(data => {
-    		setData(data)
+            console.log("Data:")
+    		setStocks(data.combinedStocks.content)
+            console.log(stocks)
     	})
     }
 
     return(
         <>
             <div style={{display: "flex", justifyContent: "space-between", padding: "0 20px", alignItems: "center"}}>
-                <h4 style={{paddingTop: 20}} className="text-center">Sélectionner l'entrepôt à visualiser</h4>
                 <div style={{width: "40%", margin: '0 auto'}}>
+                <h4 style={{paddingTop: 20}} className="text-center">Sélectionner l'entrepôt à visualiser</h4>
                     <Select 
                         options={storagesList}
                         classNamePrefix="reactSelectInput"
@@ -83,7 +88,7 @@ const StoragesList = () => {
                     <ServerSideTable 
                     ref={ServerSideTableRef}
                     columns={columns}
-                    data={data.combinedStocks}
+                    data={stocks}
                     isFilter
                     filtersList={filterColumns}
                     isSorter
